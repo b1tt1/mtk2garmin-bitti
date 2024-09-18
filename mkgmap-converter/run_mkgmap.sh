dd06b49681d7e07b42eb22cacedc93b67a3beac1
Matti Rintala <matti.rintala@tuni.fi>
2024-09-18 10:44:06 +0300

    Säädetty run_mkgmap.sh:ssa Java keon maksimikokoa

--- mkgmap-converter/run_mkgmap.sh
+++ mkgmap-converter/run_mkgmap.sh
@@ -9,8 +9,9 @@ time java -jar splitter.jar --output-dir=/splitted --max-areas=4096 --max-nodes=
 # (cat mkgmap_mtk2garmin.args;echo;cat /splitted/template.args) > /splitted/mkgmap_mtk2garmin.args
 (cat mkgmap_mtk2garmin_noparcel.args;echo;cat /splitted/template.args) > /splitted/mkgmap_mtk2garmin_noparcel.args
 # time java -jar -Xmx14G mkgmap.jar -c /splitted/mkgmap_mtk2garmin.args perus.typ
-# BITTI: time java -jar -Xmx10G mkgmap.jar -c /splitted/mkgmap_mtk2garmin_noparcel.args perus.typ
-time java -jar mkgmap.jar -c /splitted/mkgmap_mtk2garmin_noparcel.args perus.typ
+# BITTI: time java -Xmx10G -jar mkgmap.jar -c /splitted/mkgmap_mtk2garmin_noparcel.args perus.typ
+time java -Xmx8G -jar mkgmap.jar -c /splitted/mkgmap_mtk2garmin_noparcel.args perus.typ
+# time java -jar mkgmap.jar -c /splitted/mkgmap_mtk2garmin_noparcel.args perus.typ
 
 # cp perus.typ /output/mtkgarmin/perus.typ
 cp perus.typ /output/mtkgarmin_noparcel/perus.typ
